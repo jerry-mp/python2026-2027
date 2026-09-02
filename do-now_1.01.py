a85faf575275b06ccd550f56c2fcc26de65cf336
Jerry (Jared) Price <jaredmillerprice@gmail.com>
2026-09-02 09:50:28 -0700

    Add files via upload

---
+++ do-now_1.01.py
@@ -0,0 +1,37 @@
+#Do-Now 1.01
+#Python 26-27
+
+#The purpose of this lesson is to overview the use of Notepad++ and running a script through the command prompt
+#We will be covering: 1) print; 2) basic syntax
+
+#Do Now
+#Run the following scripts in your code as found below;
+# TIP: Do one prompt at a time; after testing the prompt, comment out the line using Ctrl + Q
+
+#Section 1: Test each line of code and describe the output
+#1. print(5 + 2 * 2)
+#Output:
+#2. print(2/3)
+#Output:
+#3. print(2.0*1.5)
+#Output:
+#4. print((2+3) * 10)
+#Output:
+#5. print(5.0 // 2)
+#Output:
+#6. print(5.0 % 2)
+#Were there any unexpected outputs? Did anything stick out to you?
+
+#Section 2: Test each line of code and describe the output
+#1. print(a)
+#Output:
+#2. print('a')
+#Output: 
+#3. print('a + b')
+#Output:
+#4. print('a' + 'b')
+
+#Section 3: Discussion
+#What are the basic data types we've encounterd?
+#What are the purposes of errors when we run into them?
+#What mathematical operations can we do in Python? What data can you do math with?
